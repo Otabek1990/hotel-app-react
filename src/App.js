3052081179db8fdf0d6f58e8298e248ebee2827d
Fozilov Otabek <fozilovotabek424@gmail.com>
2023-07-20 18:02:29 +0500

    app.js modified

--- src/App.js
+++ src/App.js
@@ -5,20 +5,10 @@ import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 
-  let son = 9999;
-  son=5555555;
-  let ism = "kamron";
-  const car = {
-    model: "kaptiva",
-  }
-  const products = ["kola", "fanta"];
+
 
   return (
     <div className="app">
-      <h2>Count: {son} </h2>
-      <h2>mening ismim {ism}.menda {car.model} bor</h2>
-      <h3>men {products[0]} va {products[1]} obkeldim</h3>
-
       <Navbar/>
       <Main/>
       <Footer/>
